bf964450d2dc2f3bf63583f7ed498416a0982880
Duncan Marsh <dwolfm@gmail.com>
2017-02-15 21:45:31 -0800

    update webhook route

--- server/route/github-webhook.js
+++ server/route/github-webhook.js
@@ -3,6 +3,6 @@
 let {Router} = require('express');
 let router = module.exports = new Router();
 
-router.get('/api/github-webhook', (req, res, next) => {
+router.post('/api/github-webhook', (req, res, next) => {
   res.send('Boooooyea!');
 });
